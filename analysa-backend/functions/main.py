212c858ca7d33e21cc017abbc27b01807d86557b
Aditya Iyer <itisadityaiyer@gmail.com>
2023-06-03 01:41:10 -0700

    firebase function should be working, emulator seems to timeout

--- analysa-backend/functions/main.py
+++ analysa-backend/functions/main.py
@@ -2,54 +2,99 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
-from firebase_functions import firestore_fn, https_fn
+from firebase_functions import https_fn
 from firebase_admin import initialize_app, storage
+import pandas as pd
+import numpy as np
+from scipy.stats import t
 
-initialize_app()
+
+import io
+
+app = initialize_app()
+
+DEFAULT_TARGET_COLUMNS = [
+    "watch_time",
+    "pause_count",
+    "avg_watch_time_per_video",
+    "Webpage_clicks_per_session",
+    "Videos_completed_per_session",
+]
+
+
+def generate_confidence_intervals(
+    csv_file, target_columns, treatment_group, control_group, confidence_level=0.95
+):
+    # Load the dataset from the CSV file
+    data = pd.read_csv(csv_file)
+
+    # Filter the data for the treatment and control groups
+    treatment_data = data[data["test_group"] == treatment_group]
+    control_data = data[data["test_group"] == control_group]
+
+    # Calculate the mean and standard deviation for each target column in the treatment group
+    treatment_means = treatment_data[target_columns].mean()
+    treatment_std = treatment_data[target_columns].std()
+
+    # Calculate the mean and standard deviation for each target column in the control group
+    control_means = control_data[target_columns].mean()
+    control_std = control_data[target_columns].std()
+
+    # Calculate the sample size for each group
+    treatment_size = len(treatment_data)
+    control_size = len(control_data)
+
+    # Calculate the confidence interval for each target column
+    confidence_intervals = {}
+    for column in target_columns:
+        # Calculate the standard error of the difference in means
+        se = np.sqrt(
+            (treatment_std[column] ** 2 / treatment_size)
+            + (control_std[column] ** 2 / control_size)
+        )
+
+        # Calculate the degrees of freedom
+        df = treatment_size + control_size - 2
+
+        # Calculate the critical value for the specified confidence level
+        critical_value = t.ppf((1 + confidence_level) / 2, df)
+
+        # Calculate the margin of error
+        margin_of_error = critical_value * se
+
+        # Calculate the confidence interval
+        lower_bound = (
+            treatment_means[column] - control_means[column]
+        ) - margin_of_error
+        upper_bound = (
+            treatment_means[column] - control_means[column]
+        ) + margin_of_error
+        midpoint = (lower_bound + upper_bound) / 2
+
+        confidence_intervals[column] = {
+            "lower_bound": lower_bound,
+            "upper_bound": upper_bound,
+            "midpoint": midpoint,
+        }
+
+    return confidence_intervals
 
 
 @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    print("test")
-    return https_fn.Response("Hello world!")
-
-
-# @https_fn.on_request()
-# def addmessage(req: https_fn.Request) -> https_fn.Response:
-#     """Take the text parameter passed to this HTTP endpoint and insert it into
-#     a new document in the messages collection."""
-#     # Grab the text parameter.
-#     original = req.args.get("text")
-#     if original is None:
-#         return https_fn.Response("No text parameter provided", status=400)
-
-#     firestore_client: google.cloud.firestore.Client = firestore.client()
-
-#     # Push the new message into Cloud Firestore using the Firebase Admin SDK.
-#     _, doc_ref = firestore_client.collection("messages").add({"original": original})
-
-#     # Send back a message that we've successfully written the message
-#     return https_fn.Response(f"Message with ID {doc_ref.id} added.")
-
-
-# @firestore_fn.on_document_created(document="messages/{pushId}")
-# def makeuppercase(
-#     event: firestore_fn.Event[firestore_fn.DocumentSnapshot | None],
-# ) -> None:
-#     """Listens for new documents to be added to /messages. If the document has
-#     an "original" field, creates an "uppercase" field containg the contents of
-#     "original" in upper case."""
-
-#     # Get the value of "original" if it exists.
-#     if event.data is None:
-#         return
-#     try:
-#         original = event.data.get("original")
-#     except KeyError:
-#         # No "original" field, so do nothing.
-#         return
-
-#     # Set the "uppercase" field.
-#     print(f"Uppercasing {event.params['pushId']}: {original}")
-#     upper = original.upper()
-#     event.data.reference.update({"uppercase": upper})
+    print("started")
+    bucket = storage.bucket("deep-lore-388606.appspot.com")
+    print("bucket got")
+    blob = bucket.blob("youtube_synthetic_data.csv")
+    print("blob got")
+    csv = blob.download_as_string()
+    print("csv got")
+    print(csv)
+    intervals = generate_confidence_intervals(
+        csv_file=io.StringIO(str(csv)),
+        target_columns=DEFAULT_TARGET_COLUMNS,
+        treatment_group="test",
+        control_group="control",
+    )
+    print(intervals)
+    return https_fn.Response(str(intervals))
