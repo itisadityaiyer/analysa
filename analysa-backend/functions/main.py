cb37f38c9555955dfa9ccf72cf91500e11a07f37
Aditya Iyer <itisadityaiyer@gmail.com>
2023-06-03 18:57:38 -0700

    removed logging

--- analysa-backend/functions/main.py
+++ analysa-backend/functions/main.py
@@ -27,30 +27,18 @@ DEFAULT_TARGET_COLUMNS = [
 def generate_confidence_intervals(
     csv_file, target_columns, treatment_group, control_group, confidence_level=0.95
 ):
-    print("generate_confidence_intervals: entered ")
     # Load the dataset from the CSV file
     data = read_csv(csv_file)
-
-    print("data")
-    print(data.head())
 
     # Filter the data for the treatment and control groups
     treatment_data = data[data["test_group"] == treatment_group]
     control_data = data[data["test_group"] == control_group]
 
-    print("treatment_data")
-    print(treatment_data.head())
-
-    print("control_data")
-    print(control_data.head())
-
     # Calculate the mean and standard deviation for each target column in the treatment group
-    print("generate_confidence_intervals: starting TARGET mean and std for each target")
     treatment_means = treatment_data[target_columns].mean()
     treatment_std = treatment_data[target_columns].std()
 
     # Calculate the mean and standard deviation for each target column in the control group
-    print("generate_confidence_intervals: starting CTRL mean and std for each target")
     control_means = control_data[target_columns].mean()
     control_std = control_data[target_columns].std()
 
@@ -58,13 +46,9 @@ def generate_confidence_intervals(
     treatment_size = len(treatment_data)
     control_size = len(control_data)
 
-    print("treatment size", treatment_size)
-    print("control size", control_size)
-
     # Calculate the confidence interval for each target column
     confidence_intervals = {}
     for column in target_columns:
-        print("generate_confidence_intervals: " + column)
         # Calculate the standard error of the difference in means
         se = sqrt(
             (treatment_std[column] ** 2 / treatment_size)
@@ -98,28 +82,33 @@ def generate_confidence_intervals(
     return confidence_intervals
 
 
-@https_fn.on_request(timeout_sec=60, memory=options.MemoryOption.GB_1)
+@https_fn.on_request(
+    timeout_sec=60,
+    memory=options.MemoryOption.GB_1,
+    cors=options.CorsOptions(
+        cors_origins="*",
+        cors_methods=["get", "post"],
+    ),
+)
 def request_confidence_intervals(req: https_fn.Request) -> https_fn.Response:
-    print("started")
     bucket = storage.bucket("deep-lore-388606.appspot.com")
-    print("bucket got")
     blob = bucket.blob("youtube_synthetic_data.csv")
-    print("blob got")
     csv = blob.download_as_string()
-    print("csv got")
-    print(csv)
     intervals = generate_confidence_intervals(
         csv_file=io.BytesIO(csv),
         target_columns=DEFAULT_TARGET_COLUMNS,
         treatment_group="test",
         control_group="control",
     )
-    print(intervals)
     return https_fn.Response(dumps(intervals))
 
 
-@https_fn.on_request(timeout_sec=60, memory=options.MemoryOption.GB_1)
-# @storage_fn.on_object_finalized(timeout_sec=60, memory=options.MemoryOption.GB_1)
+@https_fn.on_request(
+    cors=options.CorsOptions(
+        cors_origins="*",
+        cors_methods=["get", "post"],
+    ),
+)
 def ping_test(req: https_fn.Request) -> https_fn.Response:
     print("ping test")
     return https_fn.Response("ping test")
